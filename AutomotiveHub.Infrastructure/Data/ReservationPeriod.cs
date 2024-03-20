566fcdf69fa7b61cd5f87b222435823cb6b6fdb7
Ivan Gerov <ivangerov93@gmail.com>
2024-03-20 22:04:04 +0200

    Reservation and ReservationPeriod entities added

---
+++ AutomotiveHub.Infrastructure/Data/ReservationPeriod.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace AutomotiveHub.Infrastructure.Data
+{
+    public class ReservationPeriod
+    {
+        [Key]
+        [Comment("ReservationPeriod identifier")]
+        public int Id { get; set; }
+
+        [Required]
+        [Comment("Total days of the reservation")]
+        public int Days { get; set; }
+    }
+}
