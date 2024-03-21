b300073524b041cfdb0a52772b070aea8c20a9fc
Ivan Gerov <ivangerov93@gmail.com>
2024-03-21 21:13:10 +0200

    InitialMigration and DbSets added

---
+++ AutomotiveHub.Infrastructure/Data/AutomotiveHubDbContext.cs
@@ -0,0 +1,31 @@
+﻿using AutomotiveHub.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutomotiveHub.Data
+{
+    public class AutomotiveHubDbContext : IdentityDbContext
+    {
+        public AutomotiveHubDbContext(DbContextOptions<AutomotiveHubDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Car> Cars { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<City> Cities { get; set; }
+
+        public DbSet<Dealer> Dealers { get; set; }
+
+        public DbSet<Dealership> Dealerships { get; set; }
+
+        public DbSet<Reservation> Reservations { get; set; }
+
+        public DbSet<ReservationPeriod> ReservationPeriods { get; set; }
+
+
+    }
+}
--- AutomotiveHub.Infrastructure/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
+++ AutomotiveHub.Infrastructure/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
@@ -9,7 +9,7 @@ using System;
 
 namespace AutomotiveHub.Data.Migrations
 {
-    [DbContext(typeof(ApplicationDbContext))]
+    [DbContext(typeof(AutomotiveHubDbContext))]
     [Migration("00000000000000_CreateIdentitySchema")]
     partial class CreateIdentitySchema
     {
