acaad8818a6bc52848237e178ac42f0090329a30
Ivan Gerov <ivangerov93@gmail.com>
2024-03-27 13:05:10 +0200

    Reservation/Period added

---
+++ AutomotiveHub.Infrastructure/Data/SeedDb/ReservationConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using AutomotiveHub.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomotiveHub.Infrastructure.Data.SeedDb
+{
+    internal class ReservationConfiguration : IEntityTypeConfiguration<Reservation>
+    {
+        public void Configure(EntityTypeBuilder<Reservation> builder)
+        {
+            builder.HasData(CreateReservations());
+        }
+
+        private List<Reservation> CreateReservations()
+        {
+            return new List<Reservation>()
+            {
+                new Reservation()
+                {
+                    Id = 1,
+                    CarId = 2,
+                    StartDate = DateTime.Now,
+                    EndDate = DateTime.Now + TimeSpan.FromDays(3),
+                    TotalPrice = 1397,
+                    ReservationPeriodId = 2,
+                    IsActive = false
+                }
+            };
+        }
+    }
+}
---
+++ AutomotiveHub.Infrastructure/Data/SeedDb/ReservationPeriodConfiguration.cs
@@ -0,0 +1,56 @@
+﻿using AutomotiveHub.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomotiveHub.Infrastructure.Data.SeedDb
+{
+    internal class ReservationPeriodConfiguration : IEntityTypeConfiguration<ReservationPeriod>
+    {
+        public void Configure(EntityTypeBuilder<ReservationPeriod> builder)
+        {
+            builder.HasData(CreateReservationPeriods())
+        }
+
+        private List<ReservationPeriod> CreateReservationPeriods()
+        {
+            return new List<ReservationPeriod>()
+            {
+                new ReservationPeriod()
+                {
+                    Id = 1,
+                    Days = 2
+                },
+                new ReservationPeriod()
+                {
+                    Id = 2,
+                    Days = 3
+                },
+                new ReservationPeriod()
+                {
+                    Id = 3,
+                    Days = 5
+                },
+                new ReservationPeriod()
+                {
+                    Id = 4,
+                    Days = 7
+                },
+                new ReservationPeriod()
+                {
+                    Id = 5,
+                    Days = 10
+                },
+                new ReservationPeriod()
+                {
+                    Id = 6,
+                    Days = 30
+                },
+            };
+        }
+    }
+}
