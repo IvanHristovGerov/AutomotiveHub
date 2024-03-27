ab9e2de78377fc7ba84020e1efce1d1fd2313294
Ivan Gerov <ivangerov93@gmail.com>
2024-03-27 12:55:31 +0200

    DealershipConfiguration added

---
+++ AutomotiveHub.Infrastructure/Data/SeedDb/CityConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using AutomotiveHub.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomotiveHub.Infrastructure.Data.SeedDb
+{
+    internal class CityConfiguration : IEntityTypeConfiguration<City>
+    {
+        public void Configure(EntityTypeBuilder<City> builder)
+        {
+            builder.HasData(CreateCities());
+        }
+
+        private List<City> CreateCities()
+        {
+            return new List<City>()
+           {
+               new City
+               {
+                   Id = 1,
+                   Name = "Sofia"
+               },
+               new City
+               {
+                   Id = 2,
+                   Name = "Plovdiv"
+               },
+               new City
+               {
+                   Id = 3,
+                   Name = "Burgas"
+               },
+               new City
+               {
+                   Id = 4,
+                   Name = "Blagoevgrad"
+               },
+           };
+        }
+    }
+}
---
+++ AutomotiveHub.Infrastructure/Data/SeedDb/DealershipConfiguration.cs
@@ -0,0 +1,50 @@
+﻿using AutomotiveHub.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomotiveHub.Infrastructure.Data.SeedDb
+{
+    internal class DealershipConfiguration : IEntityTypeConfiguration<Dealership>
+    {
+        public void Configure(EntityTypeBuilder<Dealership> builder)
+        {
+            builder.HasData(CreateDealerships());
+        }
+
+        private List<Dealership> CreateDealerships()
+        {
+            return new List<Dealership>()
+            {
+                new Dealership()
+                {
+                    Id = 1,
+                    Name = "Race Culture Dealership",
+                    Address ="Sofia, bul.Botevgradsko shose 320",
+                    CityId = 1,
+                    DealerId = 3
+                },
+                new Dealership()
+                {
+                    Id = 2,
+                    Name = "LuxNWheels Dealership",
+                    Address ="Plovdiv, bul.Bulgaria 3",
+                    CityId = 2,
+                    DealerId = 1
+                },
+                new Dealership()
+                {
+                    Id = 3,
+                    Name = "Auto Class Dealership",
+                    Address ="Sofia, bul.Botevgradsko shose 320",
+                    CityId = 4,
+                    DealerId = 2
+                }
+            };
+        }
+    }
+}
