d255db41978374c1258da68a281d3dc6ab699425
Ivan Gerov <ivangerov93@gmail.com>
2024-03-27 13:21:36 +0200

    UserConfiguration added

---
+++ AutomotiveHub.Infrastructure/Data/SeedDb/UserConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using AutomotiveHub.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomotiveHub.Infrastructure.Data.SeedDb
+{
+    internal class UserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+    {
+       
+        public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+        {
+            builder.HasData(CreateUsers());
+        }
+
+        private List<ApplicationUser> CreateUsers()
+        {
+            var users = new List<ApplicationUser>();
+            var passHasher = new PasswordHasher<ApplicationUser>();
+
+            var user = new ApplicationUser()
+            {
+                Id = "8cb5bcce-a58e-4271-9a58-13811fc3c9e3",
+                UserName = "IvanGerov",
+                NormalizedUserName = "IVANGEROV",
+                Email = "ivangerov@gmail.com",
+                NormalizedEmail = "IVANGEROV@GMAIL.COM",
+                FullName = "Ivan Gerov",
+                IsActive = true
+            };
+
+            user.PasswordHash = passHasher.HashPassword(user, "gerov333");
+            users.Add(user);
+
+            return users;
+        }
+    }
+}
